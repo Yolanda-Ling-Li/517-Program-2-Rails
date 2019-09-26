0e2bc6117ea09517ab06d0b3dc05e52abf82829c
WangHngLeee <1023836798@qq.com>
2019-09-26 19:35:54 -0400

    test with library and book

--- librarysystem/app/models/library.rb
+++ librarysystem/app/models/library.rb
@@ -1,5 +1,8 @@
 class Library < ApplicationRecord
-  has_many: books
-  validation: minmum
-
+  validates :name, :presence => true
+  validates :university, :presence => true
+  validates :location, :presence => true
+  validates :maxborrowdays, :presence => true, numericality:{greater_than: 0}
+  validates :overduefine, :presence => true, numericality:{greater_than: 0}
+  has_many:books
 end
