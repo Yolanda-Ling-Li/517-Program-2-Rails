2e22ef157ad78ef036319a2f1247b5bde7d2044a
LingLi <lli46@ncsu.edu>
2019-09-26 17:30:49 -0400

    Upload image of front cover.

--- librarysystem/app/models/book.rb
+++ librarysystem/app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ApplicationRecord
   validates :language, :presence => true
   validates :published, :presence => true
   validates :edition, :presence => true
-  validates :front_cover, :presence => true
+  mount_uploader :front_cover, FrontCoverUploader
   validates :subject, :presence => true
   validates :summary, :presence => true
   validates :special, :presence => true
